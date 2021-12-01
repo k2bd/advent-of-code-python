2de765769daef4d2e9f827de07643f6a51bc643b
Kevin Duff <kevinkelduff@gmail.com>
2021-12-01 07:30:14 +0000

    Y 2021 D 1

---
+++ src/advent_of_code/y2021/d1.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from advent_of_code.util import format_solution, puzzle_input
+
+
+def count_increases(depths: List[int]) -> int:
+    """
+    Count the number of times the depth increases between two measurements
+    """
+    increases = [
+        second - first for first, second in zip(depths, depths[1:]) if second > first
+    ]
+    return len(increases)
+
+
+def count_sliding_increases(depths: List[int]) -> int:
+    """
+    Count the number of 3-member sliding sum increases in a list of measurements
+    """
+    sliding_sums = [
+        sum(measurements) for measurements in zip(depths, depths[1:], depths[2:])
+    ]
+    return count_increases(sliding_sums)
+
+
+if __name__ == "__main__":
+    depths = [int(line) for line in puzzle_input(2021, 1)]
+
+    print(format_solution(1, lambda: count_increases(depths)))
+    print(format_solution(1, lambda: count_sliding_increases(depths)))
---
+++ tests/test_y2021/test_d1.py
@@ -0,0 +1,21 @@
+from advent_of_code.y2021.d1 import count_increases, count_sliding_increases
+
+TEST_INPUT = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263,
+]
+
+
+def test_d1_p1():
+    assert count_increases(TEST_INPUT) == 7
+
+def test_d1_p2():
+    assert count_sliding_increases(TEST_INPUT) == 5
