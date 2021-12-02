c8ee2c3159a9c70e5ad1b45023c602c85bf74b0f
Kevin Duff <kevinkelduff@gmail.com>
2021-12-02 09:55:26 +0000

    Y 2021 D 2 P 1

---
+++ src/advent_of_code/y2021/d2.py
@@ -0,0 +1,39 @@
+from typing import List
+from advent_of_code.util import puzzle_input, format_solution
+from dataclasses import dataclass
+
+
+@dataclass(eq=True)
+class Position:
+    horizontal: int
+
+    depth: int
+
+
+def get_position(instructions: List[str]) -> Position:
+    """
+    Get the position of the sub, starting at 0, 0, after the following
+    instructions
+    """
+    result = Position(horizontal=0, depth=0)
+
+    for instruction in instructions:
+        direction, amount = instruction.split()
+        if direction == "forward":
+            result.horizontal += int(amount)
+        elif direction == "down":
+            result.depth += int(amount)
+        elif direction == "up":
+            result.depth -= int(amount)
+
+    return result
+
+def p1_result(instructions: List[str]) -> int:
+    position = get_position(instructions)
+    return position.depth * position.horizontal
+
+
+if __name__ == "__main__":
+    instructions = puzzle_input(2021, 2)
+
+    print(format_solution(solver_p1=lambda: p1_result(instructions), solver_p2=lambda: None))
---
+++ tests/test_y2021/test_d2.py
@@ -0,0 +1,13 @@
+from advent_of_code.y2021.d2 import Position, get_position
+
+TEST_INPUT = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2",
+]
+
+def test_d2_p1():
+    assert get_position(TEST_INPUT) == Position(horizontal=15, depth=10)
